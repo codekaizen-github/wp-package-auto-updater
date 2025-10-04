9d70b789e1179f2567dfb9c907d12401ac571fb2
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-10-04 20:16:55 +0000

    Rename meta key

--- src/AutoUpdater/AutoUpdaterPluginORASHubV1.php
+++ src/AutoUpdater/AutoUpdaterPluginORASHubV1.php
@@ -14,7 +14,7 @@ class AutoUpdaterPluginORASHubV1 implements InitializerInterface
 {
     private InitializerInterface $checkUpdateHook;
     private InitializerInterface $checkInfoHook;
-    public function __construct(string $filePath, string $baseURL, string $metaKey = 'org.codekaizen-github.wp-package-deploy-oras.wp-package-metadata', string $loggerName = 'WPPackageAutoUpdate', int|string|Level $logLevel = Level::Debug)
+    public function __construct(string $filePath, string $baseURL, string $metaKey = 'org.codekaizen-github.wp-package-deploy.wp-package-metadata', string $loggerName = 'WPPackageAutoUpdate', int|string|Level $logLevel = Level::Debug)
     {
         $logger = new Logger($loggerName, [new ErrorLogHandler(3, $logLevel)]);
         $client = new ORASHubClientPlugin($baseURL, $metaKey);
