451dddf2af2bb06bdb19543bc31cf3253df8c425
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-10-11 20:33:59 +0000

    Test

---
+++ tests/Unit/Strategy/CheckInfoStrategyTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Test
+ *
+ * @package CodeKaizen\WPPackageAutoUpdater
+ */
+
+namespace CodeKaizen\WPPackageAutoUpdaterTests\Unit\Strategy;
+
+use CodeKaizen\WPPackageAutoUpdater\Contract\Formatter\CheckInfo\CheckInfoFormatterContract;
+use CodeKaizen\WPPackageAutoUpdater\Strategy\CheckInfoStrategy;
+use CodeKaizen\WPPackageMetaProviderContract\Contract\PackageMetaContract;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use stdClass;
+
+/**
+ * Undocumented class
+ */
+class CheckInfoStrategyTest extends TestCase {
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function testSlugNotSetValid(): void {
+		$localPackageMetaProvider = Mockery::mock( PackageMetaContract::class );
+		$localPackageMetaProvider->shouldReceive( 'getShortSlug' )->with()->andReturn( 'test-plugin' );
+		$formatter = Mockery::mock( CheckInfoFormatterContract::class );
+		$logger    = Mockery::mock( LoggerInterface::class );
+		$sut       = new CheckInfoStrategy( $localPackageMetaProvider, $formatter, $logger );
+		$arg       = new stdClass();
+		$arg->slug = null;
+		$actual    = $sut->checkInfo( false, [], $arg );
+		$this->assertFalse( $actual );
+	}
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function testSlugsDoNotMatchValid(): void {
+		$localPackageMetaProvider = Mockery::mock( PackageMetaContract::class );
+		$localPackageMetaProvider->shouldReceive( 'getShortSlug' )->with()->andReturn( 'test-plugin' );
+		$formatter = Mockery::mock( CheckInfoFormatterContract::class );
+		$logger    = Mockery::mock( LoggerInterface::class );
+		$sut       = new CheckInfoStrategy( $localPackageMetaProvider, $formatter, $logger );
+		$arg       = new stdClass();
+		$arg->slug = 'other-plugin';
+		$actual    = $sut->checkInfo( false, [], $arg );
+		$this->assertFalse( $actual );
+	}
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function testPackageCheckInfoValid(): void {
+		$localPackageMetaProvider = Mockery::mock( PackageMetaContract::class );
+		$localPackageMetaProvider->shouldReceive( 'getShortSlug' )->with()->andReturn( 'test-plugin' );
+		$formatter = Mockery::mock( CheckInfoFormatterContract::class );
+		$expected  = new stdClass();
+		$formatter->shouldReceive( 'formatForCheckInfo' )->with()->andReturn( new $expected() );
+		$logger = Mockery::mock( LoggerInterface::class );
+		$logger->shouldReceive( 'debug' );
+		$sut       = new CheckInfoStrategy( $localPackageMetaProvider, $formatter, $logger );
+		$arg       = new stdClass();
+		$arg->slug = 'test-plugin';
+		$actual    = $sut->checkInfo( false, [], $arg );
+		$this->assertEquals( $expected, $actual );
+	}
+}
---
+++ tests/Unit/Strategy/CheckUpdateStrategyTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test
+ *
+ * @package CodeKaizen\WPPackageAutoUpdater
+ */
+
+namespace CodeKaizen\WPPackageAutoUpdaterTests\Unit\Strategy;
+
+use CodeKaizen\WPPackageAutoUpdater\Contract\Formatter\CheckUpdate\CheckUpdateFormatterContract;
+use CodeKaizen\WPPackageAutoUpdater\Strategy\CheckUpdateStrategy;
+use CodeKaizen\WPPackageMetaProviderContract\Contract\PackageMetaContract;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use stdClass;
+
+/**
+ * Undocumented class
+ */
+class CheckUpdateStrategyTest extends TestCase {
+}
